bfb9a480e4686272316b324c04282e5325bf6b8b
ichuuh <597280359@qq.com>
2018-08-03 16:02:47 +0800

    first commit

---
+++ findevent.py
@@ -0,0 +1,123 @@
+from datetime import datetime
+
+class FindEvent(object):
+	def __init__(self, e):
+		self.startYear = e[0].year
+		self.startMonth = e[0].month
+		self.startDay = e[0].day
+		self.startHour = e[0].hour
+		self.startMinute = e[0].minute
+
+		self.endYear = e[1].year
+		self.endMonth = e[1].month
+		self.endDay = e[1].day
+		self.endHour = e[1].hour
+		self.endMinute = e[1].minute
+
+		self.today = datetime.now()
+		self.curYear = self.today.year
+		self.curMonth = self.today.month
+		self.curDay = self.today.day
+		self.curHour = self.today.hour
+		self.curMinute = self.today.minute
+
+		self.start = datetime(self.startYear, self.startMonth, self.startDay,
+			self.startHour, self.startMinute)
+		self.end = datetime(self.endYear, self.endMonth, self.endDay, 
+			self.endHour, self.endMinute)
+
+		self.diff_cur_and_start = self.start - self.today
+		self.diff_cur_and_end = self.end - self.today
+		self.diff_start_and_end = self.end - self.start
+
+		self.eventDetails = e[3]
+
+	def get_startDomain(self, start=True):
+		hour = self.startHour
+		if self.startHour < 12:
+			phrase = "早上"
+		elif 12 <= self.startHour < 18:
+			hour = hour - 12
+			phrase = "下午"
+		else:
+			hour = hour - 12
+			phrase = "晚上"
+		return hour, phrase
+
+	def get_eventDuration(self):
+		diffDay = self.diff_start_and_end.days
+		diffSeconds = self.diff_start_and_end.seconds
+
+		year = 0
+		month = 0
+		phrase = ''
+		if diffDay > 365:
+			year = diffDay/365
+			diffDay = diffDay%365
+			phrase = str(year) + '年'
+		if diffDay > 30:
+			month = diffDay/30
+			diffDay = diffDay%30
+			phrase += str(month) + '月'
+		if diffDay > 1:
+			phrase += str(diffDay) + '天'
+		if year > 1 or month > 1 or diffDay > 1:
+			return phrase + '左右'
+
+		if diffSeconds> 3600:
+			hour = diffSeconds/3600
+			diffSeconds = diffSeconds%3600
+			phrase = str(hour) + '小时'
+		if diffSeconds > 60:
+			minute = diffSeconds/60
+			phrase += str(minute) + '分钟'
+
+		return phrase
+
+    #to-do logic has problem
+	def get_day_des(self, start=True):
+		diff = self.diff_cur_and_start if start is True else self.diff_cur_and_end
+
+		diff_day = diff.days
+		diff_sec = diff.seconds
+
+		year = ''
+		month = '这个月'
+
+		if (diff_day < 1) or ((diff_day == 1) and (diff_sec == 0)):
+			day = '明天' 
+		elif (2 < diff_day < 1) or ((diff_day == 2) and (diff_sec == 0)):
+			day = '后天'
+		elif (365 < diff_day < 365*2):
+			year = '明年' 
+		elif (30 < diff_day < 60):
+			month = '下个月'
+		else:
+			day = str(self.startDay) + '号' if start is True else str(self.endDay) + '号'
+			month = str(self.startMonth) + '月' if start is True else str(self.endMonth) + '月'
+		return year + month + day
+
+
+	def get_tense(self):
+		if (self.startMonth < self.curMonth) or (self.startDay < self.curDay):
+			tense_phrase = "在过往的这天里，您曾有过这些安排，"
+			tense_word = "已经"
+			tense_le = "了"
+		else:
+			tense_phrase = "在未来的这天里，您将有这些安排，"
+			tense_word = "将会"
+			tense_le = ""
+		return tense_phrase, tense_word, tense_le
+
+
+	def get_overall_des(self):
+		hour, domain = self.get_startDomain()
+		duration = self.get_eventDuration()
+		tense_phrase, tense_word, tense_le = self.get_tense()
+
+		phrase = tense_phrase
+		phrase += self.get_day_des(start=True) + domain + str(hour) + str(self.startMinute) + ','
+		phrase += '您' + tense_word +'参加' + tense_le + self.eventDetails + ','
+		phrase += '此项活动' + tense_word +'持续' + tense_le + duration + ','
+		phrase += '结束时间为' + self.get_day_des(start=False) + '。 '
+		return phrase
---
+++ test.py
@@ -0,0 +1,188 @@
+import os, json, time, re
+from datetime import datetime
+from datetime import timedelta
+from flask import (Flask, request, abort, jsonify)
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import extract, and_
+from findevent import FindEvent
+
+# print a nice greeting.
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+db_file = "sqlite:///{}".format(os.path.join(project_dir, "agenda.db"))
+
+# EB looks for an 'application' callable by default.
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_file
+
+db = SQLAlchemy(app)
+# add a rule for the index page.
+app.add_url_rule('/', 'index', (lambda: say_hello()))
+
+# actions = ['Add', 'Change', 'Find' ,'Delete', 'Suggestion']
+
+class Agenda(db.Model):
+	__tablename__='JDSMARTAGENDA'
+	sessID = db.Column(db.String(63), unique=True, nullable=False, primary_key=True)
+	jdID = db.Column(db.String(63), nullable=False)
+	startTime = db.Column(db.DateTime, nullable=False)
+	endTime = db.Column(db.DateTime)
+	agendaType = db.Column(db.String(26), nullable=False)
+	agendaDetail = db.Column(db.String(126))
+
+	def __init__(self, sessID=None, jdID=None, startTime=None, endTime=None, agendaType=None, agendaDetail=None):
+		self.sessID = sessID
+		self.jdID = jdID
+		self.startTime = startTime
+		self.endTime = endTime
+		self.agendaType = agendaType
+		self.agendaDetail = agendaDetail
+
+	
+@app.route('/', methods=['POST', 'GET', 'PUT'])
+def main():
+	f = open('log', 'a')
+	if not request.json:
+		abort(400)
+	f.write(json.dumps(request.json))
+	f.write('\n')
+	f.write('\n')
+	req = request.json
+	rst = process_request(req)
+	response = postResponse(rst)
+	return jsonify(response)
+
+
+def process_request(req):
+	request = req['request']
+	action = request['intent']['name']
+	jdID = req['session']['user']['userId'].replace('.','')
+	sessID = req['session']['sessionId']
+	content = request['intent']['slots']
+	if action == 'Add':
+		res = add(sessID, jdID, content)
+	elif action == 'Change':
+		res = update(jdID, content)
+	elif action == 'Delete':
+		res = delete(jdID, content)
+	elif action == 'Find':
+		res = find(jdID, content)
+	elif action == 'Suggestion':
+		res = suggest(jdID)
+	return res
+
+
+def add(sessID, jdID, content):
+	#curyear, curmonth, curday, curhour, curmin = get_curtime()
+	curtime = datetime.now()
+
+	getDate = content['AlphaDate']['value'].split('-')
+	getTime = content['StartTime']['value'].split(':')
+	duration = content['Duration']['value']
+	startyear, startmonth, startday = int(getDate[0]), int(getDate[1]), int(getDate[2])
+	starthour, startmin = int(getTime[0]), int(getTime[1])
+	startime = datetime(startyear, startmonth, startday, starthour, startmin, 0)
+	
+	diff = startime - curtime
+	if (diff.days < 0) or (diff.seconds < 0) or (diff.microseconds < 0):
+		rst = "您的行程本现在只能协助您规划未来的行程，并不支持记录以前的行程哈"
+		return rst
+
+	# check duration
+	duration_val = re.findall(r'PT(.+?)\/', duration)
+	if len(duration_val) == 0:
+		endtime = startime
+	else:
+		duration = timedelta(minutes=int(duration_val))
+		endtime = startime + duration
+	print(duration_val)
+	agendaType = content['Event']['value']
+	agenda = Agenda(sessID=sessID, jdID=jdID, agendaType=agendaType, startTime=startime, 
+		endTime=endtime, agendaDetail=agendaType)
+	try:
+		db.session.add(agenda)
+		db.session.commit()
+	except Exception as err:
+		log_error(err)
+		rst = "您的行程本出了点小问题，这条行程添加失败了..."
+		return rst
+	rst = "已帮您添加了这条行程哟，感谢您的使用"
+	return rst
+	
+
+def update(jdID):
+	return
+
+def delete(jdID, content):
+
+	return
+
+def find(jdID, content):
+	getDate = content['Date']['value'].split('-')
+	curtime = datetime.now()
+
+	year = int(getDate[0])
+	month = int(getDate[1])
+	day = int(getDate[2])
+	searchtime = datetime(year, month, day)
+	diff = curtime - searchtime
+	print("check conditions")
+	print(curtime)
+	print(searchtime)
+	print(diff)
+	if diff.days > 7:
+		rst = "不好意思哈，您的行程本目前只保存过去一星期内的行程记录。超过一星期的记录已经被自动删除了哟"
+		return rst 
+	
+	getTime = content['Time']
+	if 'value' in getTime:
+		getTime = content['Time']['value']
+	#events = db.session.query(Agenda.jdID, Agenda.startTime).filter(and_(Agenda.jdID==jdID, Agenda.startTime.between(startime, endtime))).all()
+	
+	events = db.session.query(
+		Agenda.startTime, Agenda.endTime, Agenda.agendaType, Agenda.agendaDetail).filter(and_(
+			Agenda.jdID==jdID, 
+			extract('year', Agenda.startTime) == year,
+			extract('month', Agenda.startTime) == month,
+			extract('day', Agenda.startTime) == day)).all()
+	# no record
+	if len(events) == 0:
+		rst = "您的行程本还没有记录这天的任何行程哈"
+
+	else:
+		rst = ''
+		for e in events:
+			event = FindEvent(e)
+			rst += event.get_overall_des()
+	return rst
+
+def suggest(jdID):
+	return
+
+def postResponse(rst):
+	res = {}
+	res["version"] = "1.0"
+	res["response"] = {}
+	res["response"]["output"] = {}
+	res["response"]["output"]["text"] = rst
+	res["response"]["output"]["type"] = "PlainText"
+	res["shouldEndSession"] = "true"
+	return res	
+
+def log_error(err):
+	f = open('err', 'a')
+	f.write(str(err))
+	f.write('\n')
+	f.write('\n')
+	f.close()
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    from werkzeug.contrib.fixers import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.run()
